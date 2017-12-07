d931b172c74f6637f241ba962b782378d503b1cb
mr144 <ridwan.yaa71@gmail.com>
2017-12-07 12:23:10 +0700

    init

---
+++ Assets/Scripts/ExitApp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExitApp : MonoBehaviour {
+    public Button btn_keluar;
+
+	// Use this for initialization
+	void Start () {
+        Button btn = btn_keluar.GetComponent<Button>();
+        btn.onClick.AddListener(ExitOnClick);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    void ExitOnClick()
+    {
+        Application.Quit();
+    }
+}
